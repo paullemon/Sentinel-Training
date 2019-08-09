4b9350c6bce3c5df1ecbac639f6bca9c43cdc9e8
Rachel Sharp <rachel@hashicorp.com>
2019-08-08 20:43:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ resource "tfe_sentinel_policy" "test" {
   name         = "my-policy-name"
   description  = "This policy always passes"
   organization = "my-org-name"
-  policy       = "main = rule { true }"
+  policy       = <<EOT
+import "tfplan"
+
+main = rule {
+	all tfplan.resources.aws_instance as _, instances {
+		all instances as _, r {
+			(length(r.applied.tags) else 0) > 0
+		}
+	}
+}
+EOT
   enforce_mode = "hard-mandatory"
 }
+
