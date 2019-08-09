a9e1196760a2370ccab7d611cfeb9e84a4148711
Rachel Sharp <rachel@hashicorp.com>
2019-08-08 20:47:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "tfe_sentinel_policy" "tags" {
   name         = "tags-enforced"
   description  = "Resources must be tagged"
   organization = "hashicorp-rachel"
-  policy       = "
+  policy       = <<EOT
 import "tfplan"
 
 main = rule {
@@ -11,7 +11,7 @@ main = rule {
 			(length(r.applied.tags) else 0) > 0
 		}
 	}
-}"
+}EOT
   enforce_mode = "hard-mandatory"
 }
 
