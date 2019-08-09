a73a92eef29a71cff35339136ce69b43f19d89e2
Rachel Sharp <rachel@hashicorp.com>
2019-08-08 20:35:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,4 @@ resource "tfe_policy_set" "tags" {
   description            = "A brand new policy set"
   organization           = "hashicorp-rachel"
   policies_path          = "policies/my-policy-set"
-  workspace_external_ids = ["${tfe_workspace.tags.external_id}"]
-
-  vcs_repo {
-    identifier         = "tr0njavolta/my-policy-set-repository"
-    branch             = "master"
-    ingress_submodules = false
-    oauth_token_id     = "${tfe_oauth_client.tags.oauth_token_id}"
-  }
 }
